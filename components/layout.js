774de4e284f2fa7040321801c7a55a6d28a21974
Pedro A. Moreno <pedabraham@gmail.com>
2021-02-23 19:39:50 -0600

    Solve picture issue

--- components/layout.js
+++ components/layout.js
@@ -28,7 +28,7 @@ export default function Layout({ children, home }) {
         {home ? (
           <>
             <img
-              src="/images/img_1811.jpeg"
+              src="/images/IMG_1811.jpeg"
               className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
               alt={name}
             />
@@ -39,7 +39,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <a>
                 <img
-                  src="/images/img_1811.jpeg"
+                  src="/images/IMG_1811.jpeg"
                   className={`${styles.headerImage} ${utilStyles.borderCircle}`}
                   alt={name}
                 />
